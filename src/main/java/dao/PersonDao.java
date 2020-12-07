fd45f675bf984edfac626af133c09c8559582ede
Summer Splash Sports <teghpal.singh@stonybrook.edu>
2020-12-07 03:53:03 -0500

    Finished Record a Date

---
+++ src/main/java/dao/PersonDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDao {
+
+//	Write functions in order to utilize the Person model
+
+}
